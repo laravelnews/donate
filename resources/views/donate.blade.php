4696d2cc6d7e4bfd4fcccff4008c6633dcc0ae3e
Eric Barnes <eric@ericlbarnes.com>
2015-12-31 22:33:00 -0500

    Add laravel

---
+++ resources/views/donate.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <div class="title">Laravel 5</div>
+
+        <form action="" method="POST">
+            <script
+                    src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+                    data-key="pk_test_Iu2yQ6ZTCyJqAojWtQNUSAG1"
+                    data-amount="2000"
+                    data-name="Demo Site"
+                    data-description="2 widgets ($20.00)"
+                    data-image="/128x128.png"
+                    data-locale="auto">
+            </script>
+        </form>
+    </div>
+</div>
+</body>
+</html>
